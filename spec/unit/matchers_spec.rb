6e6b71ffdc76da0a61e0dccf5cca5253ac84e10e
Svyatoslav Kryukov <s.g.kryukov@yandex.ru>
2019-10-02 18:31:23 +0300

    Add Matchers spec

---
+++ spec/unit/matchers_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe OpenapiRspec::Matchers do
+  before(:each) do
+    class Foo
+      include OpenapiRspec::Matchers
+    end
+  end
+
+  after(:each) do
+    Object.send(:remove_const, :Foo)
+  end
+
+  subject { Foo.new }
+
+  context '#validate_documentation' do
+    it 'returns new DocumentationValidator instance' do
+      allow(OpenapiRspec::DocumentationValidator).to receive(:new).and_return('result')
+
+      result = subject.validate_documentation
+
+      expect(result).to eq('result')
+      expect(OpenapiRspec::DocumentationValidator).to have_received(:new).with no_args
+    end
+  end
+
+  context '#validate_request' do
+    it 'returns new RequestValidator instance' do
+      allow(OpenapiRspec::RequestValidator).to receive(:new).and_return('result')
+
+      result = subject.validate_request(some: "params")
+
+      expect(result).to eq('result')
+      expect(OpenapiRspec::RequestValidator).to have_received(:new).with(some: "params")
+    end
+  end
+end
